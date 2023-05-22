6336f0f58b4fc9db025718c17a2ebb1dc0b0cf7e
Kevin Tackett <115435947+kevin-tackett@users.noreply.github.com>
2023-05-22 09:28:30 -0600

    Passed test.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let toCelsius = (fahrenheit -32) * 5/9
+  return Math.round(toCelsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let toFahrenheit = (celsius * 9/5 + 32)
+  return Math.round(toFahrenheit * 10) / 10
 };
 
 // Do not edit below this line
@@ -9,3 +13,4 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
