d25cb74afa19109c928a63aa4d75dd62f31f1782
Kevin Tackett <115435947+kevin-tackett@users.noreply.github.com>
2023-05-21 18:55:09 -0600

    Passed test.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseSplit = splitString.reverse();
+    let reverseArray = reverseSplit.join("");
+    return reverseArray;
 };
 
+console.log(reverseString('hello there'))
 // Do not edit below this line
 module.exports = reverseString;
