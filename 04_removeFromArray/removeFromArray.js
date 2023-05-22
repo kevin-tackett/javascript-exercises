b8c81ca920fe5b551139d6d45c9765b8802258e3
Kevin Tackett <115435947+kevin-tackett@users.noreply.github.com>
2023-05-21 19:54:16 -0600

    Passed test.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  };
 
 // Do not edit below this line
+console.log(removeFromArray([1, 2, 3, 4, 5, 6], 4))
 module.exports = removeFromArray;
