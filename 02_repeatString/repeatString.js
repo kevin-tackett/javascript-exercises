9c1c7e1ae0ecd93c6b1f2de78fd9b5471c237f42
Kevin Tackett <115435947+kevin-tackett@users.noreply.github.com>
2023-05-21 18:26:50 -0600

    Test passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let fullString = ""
+    for (let i = 0; i < num; i++) {
+        fullString += string;
+    }
+    return fullString;
 };
 
+console.log(repeatString('hey', 3))
 // Do not edit below this line
 module.exports = repeatString;
